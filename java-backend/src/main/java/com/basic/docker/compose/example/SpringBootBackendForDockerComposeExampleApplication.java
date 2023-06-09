a5dcbde9429142238f06f6cc1619e6f0d4ca1e00
Ruben Lins <desenvolvedor.ruben@gmail.com>
2023-06-09 05:41:03 -0300

    Initial version of Java Spring Boot Backend

---
+++ java-backend/src/main/java/com/basic/docker/compose/example/SpringBootBackendForDockerComposeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.basic.docker.compose.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendForDockerComposeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendForDockerComposeExampleApplication.class, args);
+	}
+
+}
